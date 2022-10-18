54b29ff329c7933d0dd618d58ce2b32c75a152c9
Carlos Villatoro <crvb0797@gmail.com>
2022-10-17 19:50:06 -0600

    Creamos las migraciones, los modelos y un seeder de usuarios
    - Clientes
    - Proyectos
    - Tareas
    
    además agregamos un seeder para crear al usuario maestro de la app

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+       $this->call([
+        UserSeeder::class
+       ]);
     }
 }
